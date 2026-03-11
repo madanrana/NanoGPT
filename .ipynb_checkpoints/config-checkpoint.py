29ba3df30673ad100024696ce1f7ae59a8f4e482
Madan Singh <madanrana964@gmail.com>
2026-03-12 02:39:19 +0530

    Added working NanoGPT implementation with training, generation, and temperature sampling

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -1,10 +1,11 @@
 # Training parameters
 
 batch_size = 32
-block_size = 64
+block_size = 16
 max_steps = 5000
 
 learning_rate = 3e-4
+temperature = 0.8
 
 # Model architecture
 
--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import torch
+import config
 import torch.nn.functional as F
 
 
@@ -10,10 +11,15 @@ def generate_text(model, start_token, decode, device, max_tokens=200):
 
     for _ in range(max_tokens):
 
-        logits, _ = model(context)
+        context_cond = context[:, -config.block_size:]
+        logits, _ = model(context_cond)
 
-        probs = F.softmax(logits[:, -1, :], dim=-1)
-
+        logits = logits[:, -1, :]      # last token prediction
+        
+        logits = logits / config.temperature  # adjust randomness
+        
+        probs = torch.softmax(logits, dim=-1)
+        
         next_token = torch.multinomial(probs, 1)
 
         context = torch.cat((context, next_token), dim=1)
